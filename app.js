d8b03fddf62bbff8cd8c9f8a5e3959914a56f676
Denislav Gavrilov <denislavgavrilov99@gmail.com>
2024-01-31 20:03:33 +0200

    update to include a secret

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 8080;
 
 // Root endpoint
 app.get('/', (req, res) => {
-  res.send('Welcome from your simple node app!');
+  res.send('Welcome from your simple node app! \n\n How are you doing, ' + process.env.YOUR_NAME + '?');
 });
 
 // Users endpoint
